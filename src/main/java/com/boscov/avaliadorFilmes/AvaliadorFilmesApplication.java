ca18dbe1e0e3502845cc85e52db033d8d64c7eae
José Marcolino <josetyu52@gmail.com>
2025-05-13 22:38:14 -0300

    Projeto Spring Boot inicial

---
+++ src/main/java/com/boscov/avaliadorFilmes/AvaliadorFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.boscov.avaliadorFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliadorFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliadorFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boscov/avaliadorFilmes/AvaliadorFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boscov.avaliadorFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliadorFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
