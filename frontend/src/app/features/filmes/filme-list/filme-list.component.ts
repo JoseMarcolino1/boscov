0bacf46cf0540770a6de8863be733f437400d579
José Marcolino <josetyu52@gmail.com>
2025-05-20 00:42:13 -0300

    feat: lista de filmes

---
+++ frontend/src/app/features/filmes/filme-list/filme-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filme-list',
+  imports: [],
+  templateUrl: './filme-list.component.html',
+  styleUrl: './filme-list.component.css'
+})
+export class FilmeListComponent {
+
+}
